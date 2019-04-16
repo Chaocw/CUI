060c6f0d575ffb9228c879d85f64a4026ca8fe81
王超 <chaowang@annoroad.com>
2019-04-16 19:46:44 +0800

    auto publish

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,16 +1,1 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import Icon from './icon/icon';
-
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e);
-};
-
-ReactDOM.render(<div>
-  <Icon
-    name="qq"
-    onClick={fn}
-    onMouseEnter={() => console.log('enter')}
-    onMouseLeave={() => console.log('leave')}
-  />
-</div>, document.querySelector('#root'));
+export {default as Icon} from './icon/icon';
