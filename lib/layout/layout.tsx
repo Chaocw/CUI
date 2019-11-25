9fdf77b9b1c5fab9ea23cac7f5949aa2dd36613d
王超 <chaowang@annoroad.com>
2019-11-25 17:40:16 +0800

    优化页面布局

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -6,26 +6,26 @@ import Sider from './sider';
 const sc = scopedClassMaker('cc-layout');
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
-	children: ReactElement | Array<ReactElement>
+  children: ReactElement | Array<ReactElement>
 }
 
 const Layout: React.FunctionComponent<Props> = (props) => {
-	const { className, ...rest } = props
-	const children = props.children as Array<ReactElement>
-	const hasAside = 'length' in children &&
-		children.reduce((result, node) => result || node.type === Sider, false)
-	return (
-		<div className={ sc({
-			'': true,
-			hasAside
-		}, { extra: className }) } { ...rest }>
-			{ props.children }
-		</div>
-	)
-}
-export default Layout
-export { Layout }
-export { default as Header } from './header'
-export { default as Content } from './Content'
-export { default as Footer } from './footer'
-export { default as Sider } from './sider'
+  const { className, ...rest } = props;
+  const children = props.children as Array<ReactElement>;
+  const hasAside = 'length' in children &&
+    children.reduce((result, node) => result || node.type === Sider, false);
+  return (
+    <div className={ sc({
+      '': true,
+      hasAside
+    }, { extra: className }) } { ...rest }>
+      { props.children }
+    </div>
+  );
+};
+export default Layout;
+export { Layout };
+export { default as Header } from './header';
+export { default as Content } from './Content';
+export { default as Footer } from './footer';
+export { default as Sider } from './sider';
