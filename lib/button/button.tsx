922873cbb62e4f3e0cb00b392151ecc0ea1b2885
王超 <chaowang@annoroad.com>
2019-12-26 13:19:06 +0800

    优化页面布局

--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -5,7 +5,7 @@ import Icon from '../icon/icon';
 
 const sc = scopedClassMaker('cc-button');
 
-interface Props extends React.ButtonHTMLAttributes<HTMLElement> {
+interface Props extends React.HTMLAttributes<HTMLElement> {
   icon?: string,
   htmlType?: string,
   type?: string,
