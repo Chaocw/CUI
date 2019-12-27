468f06abdc61ec177502124ea0a3dc781f10c539
王超 <chaowang@annoroad.com>
2019-12-27 17:55:14 +0800

    jest css modules 测不通，忽略demo

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/env bash
-npm version $1 && git push --follow-tags
+npm version $1 && \
+git push origin master:deploy
 
 #chmod +x xx  添加执行权限  sh ./xx ==>  ./xx
 #major minor patch
 #$1 == arguments[0]
+#git push --follow-tags
