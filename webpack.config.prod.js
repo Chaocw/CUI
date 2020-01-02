84023e18105bc2b1c847307d0b33c56625e841e6
王超 <chaowang@annoroad.com>
2020-01-02 14:35:47 +0800

    调试test

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,8 +4,30 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = Object.assign({}, base, {
   mode: 'production',
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
   externals: {
     react: {
       commonjs: 'react',
