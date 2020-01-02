b6ce85de599f8a546520a0a9c0cc34845eaa5cb9
王超 <chaowang@annoroad.com>
2020-01-02 11:05:17 +0800

    jest css modules 测不通，忽略demo

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,10 @@
 module.exports = {
   verbose: true,
   clearMocks: false,
-  collectCoverage: false,
   reporters: ["default"],
+
+  collectCoverage: false,
+
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleDirectories: ['node_modules'],
   globals: {
