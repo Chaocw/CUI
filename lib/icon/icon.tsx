58283c30ca8bc474998236f819a08f402d6481b2
王超 <chaowang@annoroad.com>
2020-05-21 17:29:03 +0800

    test CI

--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement} from 'react'
+import React, { ReactElement } from 'react';
 import './importIcons';
 import './icon.scss';
 import classes from '../helpers/classes';
@@ -9,14 +9,11 @@ interface IconProps extends React.SVGAttributes<SVGElement> {
   spin?: boolean
 }
 
-const Icon: React.FC<IconProps> =
-  ({className, name, component, spin, ...restProps}) => {
-    return (
-      <svg className={classes('fui-icon', className, spin ? 'spin' : '')}
-           {...restProps}>
-        {component || <use xlinkHref={`#${name}`}/>}
-      </svg>
-    );
-  };
+const Icon: React.FC<IconProps> = ({ className, name, component, spin, ...restProps }) => (
+  <svg className={ classes('fui-icon', className, spin ? 'spin' : '') }
+       { ...restProps }>
+    { component || <use xlinkHref={ `#${ name }` }/> }
+  </svg>
+);
 
 export default Icon;
