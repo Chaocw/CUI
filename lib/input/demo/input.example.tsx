145888dd20330914b207c2d865c66d97da21a2ed
王超 <chaowang@annoroad.com>
2020-06-04 15:23:37 +0800

    test CI

--- lib/input/demo/input.example.tsx
+++ lib/input/demo/input.example.tsx
@@ -1,46 +1,46 @@
-import React, { useState } from 'react'
-import Input from '../input'
+import React, { useState } from 'react';
+import Input from '../input';
 import styles from './input.example.scss';
 
 const InputExample: React.FunctionComponent = () => {
-  const [inputVal1, setInputVal1] = useState()
-  const [inputVal2, setInputVal2] = useState()
-  const [inputVal3, setInputVal3] = useState()
-  const [inputVal4, setInputVal4] = useState()
+  const [inputVal1, setInputVal1] = useState();
+  const [inputVal2, setInputVal2] = useState();
+  const [inputVal3, setInputVal3] = useState();
+  const [inputVal4, setInputVal4] = useState();
   return (
     <>
-      <div className={styles.inputExampleDemo}>
-        <p className={styles.inputExampleH1}>基本使用</p>
+      <div className={ styles.inputExampleDemo }>
+        <p className={ styles.inputExampleH1 }>基本使用</p>
         <Input
           placeholder="Basic usage"
-          onChange={(e) => setInputVal1(e.currentTarget.value)}
-          value={inputVal1}
+          onChange={ (e) => setInputVal1(e.currentTarget.value) }
+          value={ inputVal1 }
         />
       </div>
-      <div className={styles.inputExampleDemo}>
-        <p className={styles.inputExampleH1}>设置大小</p>
+      <div className={ styles.inputExampleDemo }>
+        <p className={ styles.inputExampleH1 }>设置大小</p>
         <Input
           placeholder="Basic usage"
           size="small"
-          onChange={(e) => setInputVal2(e.currentTarget.value)}
-          value={inputVal2}
+          onChange={ (e) => setInputVal2(e.currentTarget.value) }
+          value={ inputVal2 }
         />
-        <span style={{marginRight: '10px'}}></span>
+        <span style={ { marginRight: '10px' } }/>
         <Input
           placeholder="Basic usage"
-          onChange={(e) => setInputVal3(e.currentTarget.value)}
-          value={inputVal3}
+          onChange={ (e) => setInputVal3(e.currentTarget.value) }
+          value={ inputVal3 }
         />
-        <span style={{marginRight: '10px'}}></span>
+        <span style={ { marginRight: '10px' } }/>
         <Input
           size="large"
           placeholder="Basic usage"
-          onChange={(e) => setInputVal4(e.currentTarget.value)}
-          value={inputVal4}
+          onChange={ (e) => setInputVal4(e.currentTarget.value) }
+          value={ inputVal4 }
         />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default InputExample
+export default InputExample;
